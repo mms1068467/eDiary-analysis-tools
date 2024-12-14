802518048b4ce6a636d633fb889c91e96d5fcf5c
Martin Moser <56579491+mms1068467@users.noreply.github.com>
2024-12-14 11:56:16 +0100

    Update 02_Stress_Analysis (sqlite).py fixing Kyriakou geo-merging

--- src/pages/02_Stress_Analysis (sqlite).py
+++ src/pages/02_Stress_Analysis (sqlite).py
@@ -354,7 +354,11 @@ if uploaded_sqlite_file is not None:
                 final_MOS_output_geo['time_iso'] = pd.to_datetime(final_MOS_output_geo['time_iso'])
                 location_data['time_iso'] = pd.to_datetime(location_data['time_iso'])
 
-                hour_diff_location_data = check_same_hour(location_data, final_MOS_output, datetime_column='time_iso')
+                st.write(location_data)
+
+                st.write(final_MOS_output_geo)
+
+                hour_diff_location_data = check_same_hour(location_data, final_MOS_output_geo, datetime_column='time_iso')
                 
                 if np.sum(hour_diff_location_data) > 0:
                     location_data["time_iso"] = location_data["time_iso"] - pd.Timedelta(hours=1)
@@ -363,7 +367,7 @@ if uploaded_sqlite_file is not None:
 
                 # Merge the dataframes based on matching seconds
                 if 'time_iso' in location_data.columns:
-                    merged_df = merge_on_matching_seconds(final_MOS_output, location_data, datetime_column='iso_time')
+                    merged_df = merge_on_matching_seconds(final_MOS_output_geo, location_data, datetime_column='iso_time')
                     st.write(merged_df)
                 else:
                     st.write("Merge problem - timestamp compatibility issue")
